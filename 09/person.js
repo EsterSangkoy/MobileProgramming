ec9f5ab38f62c713c7b60a8b97b044cc7d0179a4
EsterSangkoy <s21910288@student.unklab.ac.id>
2021-03-14 23:13:00 +0800

    Pertemuan 9

---
+++ 09/person.js
@@ -0,0 +1,17 @@
+const fullName = "Ester Sangkoy";
+
+ const person = {
+    firstName: "Ester",
+    lastName: "Sangkoy",
+    age : 19,
+
+};
+
+ const display = ({firstName,lastName}) => {
+    console.log(`Hello ${firstName}  ${lastName}`);
+};
+
+// export {fullName,person,display};
+
+//Export AS and Export AS
+export {fullName,person,display};
